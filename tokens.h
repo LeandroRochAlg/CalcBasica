2d949c9438ad03329f9cc1adb539489c78fbd568
LeandroRochAlg <leandrochaju@hotmail.com>
2024-10-16 19:26:12 -0300

    feat: Adicionar analisador léxico flex e exemplos

---
+++ tokens.h
@@ -0,0 +1,35 @@
+#ifndef TOKENS_H
+#define TOKENS_H
+
+/* Definição dos tokens retornados pelo analisador léxico */
+#define PROGRAMA_TOK 1
+#define INICIO_TOK 2
+#define FIM_TOK 3
+#define DECLARACOES_TOK 4
+#define ALGORITMO_TOK 5
+#define INTEIRO_TOK 6
+#define REAL_TOK 7
+#define CARACTER_TOK 8
+#define CADEIA_TOK 9
+#define LISTA_INT_TOK 10
+#define LISTA_REAL_TOK 11
+#define LEIA_TOK 12
+#define ESCREVA_TOK 13
+#define SE_TOK 14
+#define ENTAO_TOK 15
+#define FIM_SE_TOK 16
+#define ENQUANTO_TOK 17
+#define FIM_ENQUANTO_TOK 18
+#define ATRIBUICAO_TOK 19
+#define MAIS_TOK 20
+#define MENOS_TOK 21
+#define MULTIPLICA_TOK 22
+#define DIVIDE_TOK 23
+#define RESTO_TOK 24
+#define MAIOR_TOK 25
+#define IGUAL_TOK 26
+#define NUM_INT_TOK 27
+#define NUM_REAL_TOK 28
+#define IDENT_TOK 29
+
+#endif
